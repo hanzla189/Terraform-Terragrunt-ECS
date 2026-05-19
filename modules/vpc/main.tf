cbd3d6ff4eba96368552cf38cf437f70e700cf7f
hanzla189 <hanzlatanveer189@gmail.com>
2026-05-19 04:42:33 -0700

    Update main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,6 +1,6 @@
 resource "aws_vpc" "this" {
   cidr_block           = var.vpc_cidr
-  enable_dns_hostnames = truef
+  enable_dns_hostnames = true
   enable_dns_support   = true
 
   tags = merge(local.common_tags, {
